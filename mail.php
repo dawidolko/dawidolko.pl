6c8c0e1fe1cbc150be5b6ec9dc79908ce49a1e32
dawidolko <dawid_olko@outlook.com>
2025-02-13 00:06:05 +0100

    update

--- mail.php
+++ mail.php
@@ -1,10 +1,9 @@
 <?php
-// Włączanie raportowania błędów dla debugowania, wyłącz w środowisku produkcyjnym
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-header('Access-Control-Allow-Origin: *'); // Dla zapytań CORS, zastąp '*' rzeczywistą domeną, jeśli to konieczne
-header('Content-Type: text/plain; charset=utf-8'); // Ustaw odpowiedni typ zawartości
+header('Access-Control-Allow-Origin: *'); 
+header('Content-Type: text/plain; charset=utf-8'); 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = strip_tags(trim($_POST["name"]));
@@ -12,26 +11,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = strip_tags(trim($_POST["subject"]));
     $message = trim($_POST["message"]);
 
-    // Walidacja wejścia
     if (empty($name) || empty($subject) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        // Ustawienie kodu odpowiedzi na 400 i wyjście z błędem
         http_response_code(400);
         echo "Please complete the form and try again.";
         exit;
     }
 
-    // Odbiorca wiadomości
     $recipient = "poczta@dawidolko.pl";
     
-    // Budowa treści wiadomości
     $email_content = "Name: $name\n";
     $email_content .= "Email: $email\n\n";
     $email_content .= "Message:\n$message\n";
 
-    // Budowa nagłówków e-mail
     $email_headers = "From: $name <$email>";
 
-    // Wysyłanie wiadomości
     if (mail($recipient, $subject, $email_content, $email_headers)) {
         http_response_code(200);
         echo "Thank You! Your message has been sent.";
@@ -40,7 +33,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Oops! Something went wrong and we couldn't send your message.";
     }
 } else {
-    // Nieprawidłowa metoda żądania
     http_response_code(403);
     echo "There was a problem with your submission, please try again.";
 }
--- src/js/main.js
+++ src/js/main.js
@@ -6,49 +6,49 @@ const navOpen = document.querySelector(".nav__open");
 const navAll = document.querySelectorAll(".nav__link");
 
 navBtn.addEventListener("click", () => {
-	navLinks.classList.toggle("nav__links--active");
-	navOpen.classList.toggle("nav__hide");
+  navLinks.classList.toggle("nav__links--active");
+  navOpen.classList.toggle("nav__hide");
 });
 
 navAll.forEach((el) => {
-	el.addEventListener("click", () => {
-		navLinks.classList.remove("nav__links--active");
-		navOpen.classList.toggle("nav__hide");
-	});
+  el.addEventListener("click", () => {
+    navLinks.classList.remove("nav__links--active");
+    navOpen.classList.toggle("nav__hide");
+  });
 });
 
 window.addEventListener("scroll", () => {
-	if (window.scrollY > 80) {
-		nav.classList.add("nav__bgc");
-	} else {
-		nav.classList.remove("nav__bgc");
-	}
+  if (window.scrollY > 80) {
+    nav.classList.add("nav__bgc");
+  } else {
+    nav.classList.remove("nav__bgc");
+  }
 });
 
 //**********REVEAL**********
 const reveal = () => {
-	const elements = document.querySelectorAll(".move, .moves");
-	const windowHeight = window.innerHeight;
-	const revealPoint = 50;
-	const revealPointSmall = 20;
+  const elements = document.querySelectorAll(".move, .moves");
+  const windowHeight = window.innerHeight;
+  const revealPoint = 50;
+  const revealPointSmall = 20;
 
-	elements.forEach((element) => {
-		const revealTop = element.getBoundingClientRect().top;
+  elements.forEach((element) => {
+    const revealTop = element.getBoundingClientRect().top;
 
-		if (revealTop < windowHeight - revealPoint) {
-			element.classList.add("active");
-		}
+    if (revealTop < windowHeight - revealPoint) {
+      element.classList.add("active");
+    }
 
-		if (
-			(element.classList.contains("move") &&
-				revealTop < windowHeight - revealPointSmall) ||
-			(element.classList.contains("moves") &&
-				revealTop < windowHeight - revealPoint)
-		) {
-			element.style.opacity = "1";
-			element.style.transform = "translateY(0)";
-		}
-	});
+    if (
+      (element.classList.contains("move") &&
+        revealTop < windowHeight - revealPointSmall) ||
+      (element.classList.contains("moves") &&
+        revealTop < windowHeight - revealPoint)
+    ) {
+      element.style.opacity = "1";
+      element.style.transform = "translateY(0)";
+    }
+  });
 };
 window.addEventListener("scroll", reveal);
 
@@ -58,29 +58,29 @@ const contactMessage = document.querySelector(".info__message");
 const aboutMessage = document.querySelector(".about__message");
 
 emailLinks.forEach((link) => {
-	link.addEventListener("click", async (event) => {
-		event.preventDefault();
-		const email = link.getAttribute("href").replace("mailto:", "");
+  link.addEventListener("click", async (event) => {
+    event.preventDefault();
+    const email = link.getAttribute("href").replace("mailto:", "");
 
-		try {
-			if (window.innerWidth >= 768) {
-				await navigator.clipboard.writeText(email);
-				if (link.closest(".contact")) {
-					contactMessage.style.display = "block";
-					setTimeout(() => {
-						contactMessage.style.display = "none";
-					}, 2000);
-				} else if (link.closest(".about")) {
-					aboutMessage.style.display = "block";
-					setTimeout(() => {
-						aboutMessage.style.display = "none";
-					}, 2000);
-				}
-			} else {
-				window.location.href = `mailto:${email}`;
-			}
-		} catch (err) {}
-	});
+    try {
+      if (window.innerWidth >= 768) {
+        await navigator.clipboard.writeText(email);
+        if (link.closest(".contact")) {
+          contactMessage.style.display = "block";
+          setTimeout(() => {
+            contactMessage.style.display = "none";
+          }, 2000);
+        } else if (link.closest(".about")) {
+          aboutMessage.style.display = "block";
+          setTimeout(() => {
+            aboutMessage.style.display = "none";
+          }, 2000);
+        }
+      } else {
+        window.location.href = `mailto:${email}`;
+      }
+    } catch (err) {}
+  });
 });
 
 //**********FOOTER DATE **********
